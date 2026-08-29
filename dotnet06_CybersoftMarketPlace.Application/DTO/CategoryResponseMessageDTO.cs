513ba7b04ec751faa753bb84c6df5b377cfaaeef
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-08-29 23:35:04 +0700

    feat(category): add CategoryResponseMessageDto

---
+++ dotnet06_CybersoftMarketPlace.Application/DTO/CategoryResponseMessageDTO.cs
@@ -0,0 +1,11 @@
+public static class CategoryResponseMessageDTO
+{
+    public const string CreateSuccess =
+        "Create category successfully";
+
+    public const string CategoryAlreadyExists =
+        "Category already exists";
+
+    public const string CreateFailed =
+        "Create category failed";
+}
